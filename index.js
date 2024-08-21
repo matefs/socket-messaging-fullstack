6175a83eb24f342df373e28e9cf2959a8b299f55
Mateus Schverz <matefs8569@gmail.com>
2024-08-21 12:04:36 -0300

    refactor: removed unecessary comments

--- index.js
+++ index.js
@@ -17,10 +17,8 @@ const io = socketio(server, {
   },
 });
 
-// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve the index.html file for the root route
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
